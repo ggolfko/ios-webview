1cf4b81415cb69e1fc2e2537e079732f5538c042
IT GURU <itguru@its-imac.local>
2018-01-25 16:03:44 +0700

    Initial Commit

---
+++ Beecard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beecard
+//
+//  Created by IT GURU on 25/1/61.
+//  Copyright © พ.ศ. 2561 PiC-Phuket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
